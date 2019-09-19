9ec851a56106afd8f0808e735e0a638e06a75621
Adrian Bragg <adrianbragg0@gmail.com>
2019-09-19 10:55:30 -0500

    Finished the UI started on JavaScript

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+//Function that is executed when the button is clicked
+//We are passing an anonymous function (a function without a name to the button's onclick handler when it's clicked)
+document.getElementById("calculateTip").onclick = function() {
+    console.log("Button was clicked");
+} 
